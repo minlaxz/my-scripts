28586652e0c372015accde793fab174af8aba430
minlaxz <minminlaxz@gmail.com>
2021-08-11 00:26:12 +0630

    cra installer is migrated into this

--- minlaxz.sh
+++ minlaxz.sh
@@ -24,13 +24,52 @@ warning_output() {
 
 error_output() {
     echo -e "${ERROR}Error : $1 ${RESET}"
+    return 1
 }
 
 setup_laxzhome() {
     LAXZHOME=$HOME/.laxz
     mkdir -p $LAXZHOME
     test -f $LAXZHOME/laxzrc || touch $LAXZHOME/laxzrc
-    test -f $LAXZHOME/laxz.alias || touch $LAXZHOME/laxz.alias
+    # test -f $LAXZHOME/laxz.alias || touch $LAXZHOME/laxz.alias
+}
+
+install_cra() {
+    heading_output "Installing CRA ..."
+    check_and_remove_cra() {
+        if [ -f $LAXZHOME/cra.sh ]; then
+            warning_output "CHECK - found cra, removing ... "
+            /usr/bin/rm -rf $LAXZHOME/cra.sh
+        fi
+        warning_output "cra is removed."
+    }
+    download_cra() {
+        curl -fsSL https://raw.githubusercontent.com/minlaxz/cra-by-noob/main/cra.sh -o $LAXZHOME/cra.sh
+        chmod 755 $LAXZHOME/cra.sh
+        descriptionOutput "DONE - downloaded cra."
+    }
+    update_rc_cra() {
+        sed -i '/alias cra/d' $LAXZHOME/.laxzrc # clean alias about cra in laxzrc
+        warning_output "DONE - cra alias is cleaned from laxzrc"
+
+        if [ -f $LAXZHOME/cra.sh ]; then
+            # cra exists
+            cat <<EOF >>$LAXZHOME/.laxzrc
+alias cra="$HOME/.laxz/cra.sh"
+EOF
+            descriptionOutput "DONE - cra alias is added to laxzrc"
+        fi
+    }
+    finish_up_cra() {
+        sed -i '/source $HOME\/\.laxz\/\.laxzrc/d' $HOME/.zshrc
+        warning_output "DONE - laxzrc is cleaned from zshrc"
+        echo "source $HOME/.laxz/.laxzrc" >>$HOME/.zshrc
+        description_utput "ya're good to go!\nJus run 'cra'"
+    }
+    check_and_remove_cra
+    download_cra
+    update_rc_cra
+    finish_up_cra
 }
 
 installer_help() {
@@ -48,15 +87,18 @@ installer() {
         # Install laxz
         echo "Installed laxz"
         ;;
-    "cra")
-        heading_output "Installing CRA"
-        # Install cra
-        echo "Installed CRA"
-        ;;
-    *)
-        installer_help $1
-        ;;
+    "cra") install_cra ;;
+    *) installer_help $1 ;;
     esac
+}
+
+check_shell() {
+    # ps | grep $(echo $$) | awk '{ print $4 }'
+    base_shell=$(basename $SHELL)
+    # basename $0
+    if [[ $base_shell != "zsh" ]]; then
+        error_output "Other shell is not supported yet." # return 1
+    fi
 }
 
 main_help() {
@@ -68,15 +110,17 @@ main_help() {
 }
 
 main() {
+    check_shell
+    setup_laxzhome
     case $1 in
     "" | "-h" | "--help")
         cat <<EOF >&2
 Usage: $0 [options]
     Options:
         -h, --help      show this help message and exit
-        -i, --install   install something
-        -u, --uninstall uninstall something
-        -X              Clean up everything
+        -i, --install   install <laxz, cra>
+        -u, --uninstall uninstall <laxz, cra>
+        -X              Clean up everything about laxz
 EOF
 
         ;;
@@ -87,7 +131,7 @@ EOF
         echo "Uninstalling"
         ;;
     "-X")
-        warning_output "Cleaning up everything."
+        warning_output "Cleaning up everything about laxz."
         ;;
     *)
         main_help "$1"
