be8b5755695580b968119d7b2db34f555e682223
minlaxz <minminlaxz@gmail.com>
2021-08-21 21:38:37 +0630

    Docker install option added

--- sys_setup.sh
+++ sys_setup.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+docker_install_options() {
+    cat <<EOF 2>&1
+    This script will install the following:
+    - Docker
+    - Docker Compose
+EOF
+
+install_options() {
+    cat <<EOF 2>&1
+    1) Install Docker and Docker Compose
+    2) Exit
+EOF
+
+}
+
+}
+
 main() {
     # Check if the user is root
     # if [ "$(id -u)" != "0" ]; then
@@ -20,6 +37,20 @@ main() {
     fi
 
     echo "All check passed, I am running..."
+    install_options
+    read -p "Please select an option: " option
+    case $option in
+        1)
+            docker_install_options
+            ;;
+        2)
+            exit 0
+            ;;
+        *)
+            echo "Invalid option" 1>&2
+            exit 1
+            ;;
+    esac
 }
 
 main
