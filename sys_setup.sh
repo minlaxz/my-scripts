b70f36b54a67cc689219aa5cb1d2b3342acf0326
minlaxz <minminlaxz@gmail.com>
2021-08-21 21:32:45 +0630

    Installer initial

--- sys_setup.sh
+++ sys_setup.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
-echo "Hello from sys_setup.sh"
+main() {
+    # Check if the user is root
+    # if [ "$(id -u)" != "0" ]; then
+    #     echo "This script must be run as root" 1>&2
+    #     exit 1
+    # fi
+
+    # Check if the script is running on Ubuntu
+    if [ -f /etc/lsb-release ]; then
+        . /etc/lsb-release
+        if [ "$DISTRIB_ID" != "Ubuntu" ]; then
+            echo "This script is only for Ubuntu" 1>&2
+            exit 1
+        fi
+    else
+        echo "This script is only for Ubuntu" 1>&2
+        exit 1
+    fi
+
+    echo "All check passed, I am running..."
+}
+
+main
